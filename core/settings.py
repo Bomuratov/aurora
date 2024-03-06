b2d6b877eb6d60c5dd14dbd4fe7483f81c348990
Умид Бомуратов <antikko99@gmail.com>
2024-03-06 15:41:09 +0500

    ------

--- core/settings.py
+++ core/settings.py
@@ -109,9 +109,9 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 REST_FRAMEWORK = {
    "DEFAULT_AUTHENTICATION_CLASSES": (
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        # "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        # 'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
     ),
     "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
     # 'DEFAULT_AUTHENTICATION_CLASSES': [
