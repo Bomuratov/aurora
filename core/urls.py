0bab005ffdb05620bb96b822049fb4e2c4d85d2e
Умид Бомуратов <antikko99@gmail.com>
2024-03-06 15:39:26 +0500

    --

--- core/urls.py
+++ core/urls.py
@@ -10,7 +10,7 @@ from menu_app.views import CustomTokenObtain
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('api/auth', include("rest_framework.urls")),
+    path('api/auth/', include("rest_framework.urls")),
     path('api/token/', CustomTokenObtain.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path(
