b58ca8324fb222ead72cc5331daaf986546cdac0
Умид Бомуратов <antikko99@gmail.com>
2024-03-24 18:08:15 +0500

    xuyozna

--- core/settings_prod.py
+++ core/settings_prod.py
@@ -1,13 +1,13 @@
-#from .settings import *
-#DEBUG = True
+from .settings import *
+DEBUG = True
 
-#DATABASES = {
-#    "default": {
-#        "ENGINE": "django.db.backends.postgresql",
-#        "NAME": "aurora",
-#        "USER": "aurora",
-#        "PASSWORD": "N4:>g0z}r2Xx",
-#        "HOST": "localhost",
-#        "PORT": "5432",
-#    }
-#}
+DATABASES = {
+   "default": {
+       "ENGINE": "django.db.backends.postgresql",
+       "NAME": "aurora",
+       "USER": "aurora",
+       "PASSWORD": "N4:>g0z}r2Xx",
+       "HOST": "localhost",
+       "PORT": "5432",
+   }
+}
