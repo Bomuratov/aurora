592c7a14b97b498038ddb183a844d5375475e9e7
Umid Bomuratov <antikko99@gmail.com>
2024-06-08 09:44:09 +0500

    ----

--- menu_app/models.py
+++ menu_app/models.py
@@ -26,8 +26,13 @@ def upload_logo_rest(instance, file):
     return "{0}/logo/{1}".format(instance.name, file)
 
 
-def upload_path_promo(instance, file):
-    return "{0}/promo/{1}/{2}".format(instance.restaurant.name, instance.name, file)
+def upload_path_promo(instance, filename):
+    file = filename.rfind(".")
+    formatt = filename[file:]
+    name = instance.name + formatt
+    return "{0}/promo/{1}/{2}".format(
+        instance.restaurant.name, instance.name, name
+    )
 
 
 
