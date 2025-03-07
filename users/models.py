5d4daf6939cb07f9f1edc50d7a0323a60b6a1b48
Bomuratov <antikko99@gmail.com>
2025-03-08 00:28:27 +0500

    пуш для сервера

--- users/models.py
+++ users/models.py
@@ -1,46 +1,46 @@
-# from django.db import models
-# from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
-# from users.utils.phone_validator import UZB_PHONE_VALIDATOR
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from users.utils.phone_validator import UZB_PHONE_VALIDATOR
 
 
-# class UserManager(BaseUserManager):
+class UserManager(BaseUserManager):
 
-#     def create_user(self, phone, password=None):
-#         if not phone:
-#             raise ValueError("User must have a username")
-#         user = self.model(phone=phone)
-#         user.set_password(password)
-#         user.save(using=self._db)
-#         return user
+    def create_user(self, phone, password=None):
+        if not phone:
+            raise ValueError("User must have a username")
+        user = self.model(phone=phone)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-#     def create_superuser(self, phone, password):
-#         user = self.create_user(phone=phone, password=password)
-#         user.is_active = True
-#         user.is_staff = True
-#         user.is_user = True
-#         user.is_vendor = True
-#         user.is_superuser = True
-#         user.save(using=self._db)
-#         return user
-
-
+    def create_superuser(self, phone, password):
+        user = self.create_user(phone=phone, password=password)
+        user.is_active = True
+        user.is_staff = True
+        user.is_user = True
+        user.is_vendor = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
 
 
-# class User(AbstractBaseUser, PermissionsMixin):
-#     first_name = models.CharField(max_length=255, null=True, blank=True)
-#     last_name = models.CharField(max_length=255, null=True, blank=True)
-#     email = models.EmailField(null=True, blank=True)
-#     phone=models.CharField(max_length=14, null=True, blank=True, unique=True, validators=[UZB_PHONE_VALIDATOR])
-#     telegram_id = models.PositiveBigIntegerField(null=True, blank=True)
-#     user_registered_at = models.DateTimeField(auto_now_add=True, editable=False, null=True)
-#     is_active = models.BooleanField(default=False)
-#     is_staff = models.BooleanField(default=False)
-#     is_user = models.BooleanField(default=False)
-#     is_vendor = models.BooleanField(default=False)
 
-#     USERNAME_FIELD = "phone"
-#     objects = UserManager()
 
-#     def __str__(self):
-#         return f"{self.first_name} {self.last_name} – {self.phone}"
+class User(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(max_length=255, null=True, blank=True)
+    last_name = models.CharField(max_length=255, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    phone=models.CharField(max_length=14, null=True, blank=True, unique=True, validators=[UZB_PHONE_VALIDATOR])
+    telegram_id = models.PositiveBigIntegerField(null=True, blank=True)
+    user_registered_at = models.DateTimeField(auto_now_add=True, editable=False, null=True)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_user = models.BooleanField(default=False)
+    is_vendor = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "phone"
+    objects = UserManager()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} – {self.phone}"
 
--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,16 @@
-# from django.urls import path
-# from users.views import register, user_token
-# from rest_framework_simplejwt.views import TokenRefreshView
+from django.urls import path
+from users.views import register, user_token
+from rest_framework_simplejwt.views import TokenRefreshView
 
 
-# urlpatterns = [
-#     path("register", register.UserRegisterView.as_view({"post":"create"}), name="register"),
-#     path("user", register.UserRegisterView.as_view({"get":"list"}), name="user-list"),
-#     path("user/<int:pk>", register.UserRegisterView.as_view({"get":"retrieve"}), name="user-get"),
-#     path("user/<int:pk>", register.UserRegisterView.as_view({"put":"update"}), name="user-update"),
-#     path("user/<int:pk>", register.UserRegisterView.as_view({"delete":"destroy"}), name="user-delete"),
-#     path("user/login", user_token.UserTokenView.as_view(), name="user-login"),
-#     path("refresh", TokenRefreshView.as_view(), name="refresh-token"),
-# ]
+urlpatterns = [
+    path("register", register.UserRegisterView.as_view({"post":"create"}), name="register"),
+    path("user", register.UserRegisterView.as_view({"get":"list"}), name="user-list"),
+    path("user/<int:pk>", register.UserRegisterView.as_view({"get":"retrieve"}), name="user-get"),
+    path("user/<int:pk>", register.UserRegisterView.as_view({"put":"update"}), name="user-update"),
+    path("user/<int:pk>", register.UserRegisterView.as_view({"delete":"destroy"}), name="user-delete"),
+    path("user/login", user_token.UserTokenView.as_view(), name="user-login"),
+    path("refresh", TokenRefreshView.as_view(), name="refresh-token"),
+]
+
+
