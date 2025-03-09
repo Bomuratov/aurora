1e02dce5636c293b7e2705ee1e2c539222037309
Bomuratov <antikko99@gmail.com>
2025-03-09 16:31:39 +0500

    refactor serializers - add category and promo serializers

--- menu_app/serializer/promo_serializer.py
+++ menu_app/serializer/promo_serializer.py
@@ -6,8 +6,6 @@ from menu_app.models import Promo, Restaurant
 class PromoSerializer(serializers.ModelSerializer):
     created_by = serializers.HiddenField(default=serializers.CurrentUserDefault())
     update_by = serializers.HiddenField(default=serializers.CurrentUserDefault())
-    restaurant = serializers.PrimaryKeyRelatedField(queryset=Restaurant.objects.all())
-    photo = serializers.FileField(required=False)
     
     class Meta:
         model = Promo
