41c2c8081b84867a15642ea25b6520eed93314e6
Bomuratov <antikko99@gmail.com>
2025-03-16 17:18:23 +0500

    add multiply menu add send to server

--- core/settings_dev.py
+++ core/settings_dev.py
@@ -8,11 +8,21 @@ DEBUG = True
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "aurora",
+        "NAME": "koyebdb",
         "USER": "aurora",
-        "PASSWORD": "admin",
-        "HOST": "localhost",
+        "PASSWORD": "npg_QGriLCUpA58c",
+        "HOST": "ep-empty-term-a22mzt2a.eu-central-1.pg.koyeb.app",
         "PORT": "5432",
     }
 }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "aurora",
+#         "USER": "aurora",
+#         "PASSWORD": "admin",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
 
