a815b131c5dd8161f52b67a2f5972369ff6852be
Bomuratov <antikko99@gmail.com>
2025-04-21 01:11:09 +0500

    update UserAdmin to include phone in list_display; modify __str__ method in User model to exclude phone

--- users/admin.py
+++ users/admin.py
@@ -4,7 +4,7 @@ from .models import User, UserLocation
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("first_name", "last_name", "is_user",  "is_vendor", "is_active")
+    list_display = ("first_name", "last_name", "phone", "is_user",  "is_vendor", "is_active")
     list_display_links = ("first_name",)
     list_editable = ("is_active", "is_user",  "is_vendor",)
     list_per_page = 30
@@ -13,6 +13,8 @@ class UserAdmin(admin.ModelAdmin):
 
 
 class LocationAdmin(admin.ModelAdmin):
+    list_display = ("user","address", "is_active",)
+    list_editable = ("is_active",)
     list_filter = ("user",)
 
 
--- users/models.py
+++ users/models.py
@@ -43,7 +43,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name} – {self.phone}"
+        return f"{self.first_name} {self.last_name}"
 
 
 class UserLocation(models.Model):
