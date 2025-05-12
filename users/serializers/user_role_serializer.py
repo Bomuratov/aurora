14e4a85306686b890b6cc2e6e2486766bf9204ed
Bomuratov <antikko99@gmail.com>
2025-05-12 14:01:49 +0500

    feat(user_role): add label field to UserRoleSerializer for better role representation

--- users/serializers/user_role_serializer.py
+++ users/serializers/user_role_serializer.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
 from users.models import UserRole
+from users.utils.constants import ROLES
 
 
 class UserRoleSerializer(serializers.ModelSerializer):
+    label = serializers.SerializerMethodField()
+
     class Meta:
         model = UserRole
-        fields = ["id", "role"]
+        fields = ["id", "role", "label"]
+
+    def get_label(self, instance):
+        role_value = instance.role
+        role_dict = dict(ROLES)
+        return role_dict.get(role_value, None)
