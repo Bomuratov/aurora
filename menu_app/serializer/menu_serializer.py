b09389613bea48f9b22d0398f1357c649208b78f
Bomuratov <antikko99@gmail.com>
2025-05-25 00:50:11 +0500

    fix(menu_serializer): update options field to be read-only and comment out options creation logic

--- menu_app/serializer/menu_serializer.py
+++ menu_app/serializer/menu_serializer.py
@@ -7,7 +7,7 @@ from menu_app.serializer.options_serializer import OptionsSerializer
 class MenuSerializer(serializers.ModelSerializer):
     # created_by = serializers.HiddenField(default=serializers.CurrentUserDefault())
     # update_by = serializers.HiddenField(default=serializers.CurrentUserDefault())
-    options = OptionsSerializer(many=True, required=False)
+    options = OptionsSerializer(many=True, required=False, read_only=True)
     
     class Meta:
         model = Menu
@@ -30,8 +30,8 @@ class MenuSerializer(serializers.ModelSerializer):
         options = validated_data.pop('options', [])
         cat_id = validated_data["category"]
         data = Menu.objects.create(**validated_data)
-        for option in options:
-            Options.objects.create(menu=data, **option)
+        # for option in options:
+        #     Options.objects.create(menu=data, **option)
         if cat_id:
             instance = bool(Menu.objects.filter(category=cat_id, is_active=True))  # тут возвращается либо Тру либо Фолс
             cat_id.is_active = instance # Берем инстанс Категории
